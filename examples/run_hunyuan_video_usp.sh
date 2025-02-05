1088731eebd53982c3520227ad2bba863a6148ea
Marvin Tsai <62472426+mqhc2020@users.noreply.github.com>
2025-02-05 06:22:01 +0000

    sweep all combinations

--- examples/run_hunyuan_video_usp.sh
+++ examples/run_hunyuan_video_usp.sh
@@ -10,6 +10,7 @@ MODEL_ID="tencent/HunyuanVideo"
 WARMUP_STEPS=0
 
 mkdir -p ./results
+mkdir -p ./logs
 
 # CogVideoX specific task args
 TASK_ARGS="--height 720 --width 1280 --num_frames 129"
@@ -24,7 +25,8 @@ N_GPUS=8
 #OUTPUT_ARGS="--output_type latent"
 # PARALLLEL_VAE="--use_parallel_vae"
 ENABLE_TILING="--enable_tiling"
-#ENABLE_MODEL_CPU_OFFLOAD="--enable_model_cpu_offload"
+ENABLE_SLICING="--enable_slicing"
+ENABLE_MODEL_CPU_OFFLOAD="--enable_model_cpu_offload"
 # COMPILE_FLAG="--use_torch_compile"
 
 if [[ "$PROFILE_DIFFUSION" == "1" ]]; then
@@ -39,9 +41,47 @@ else
     PROFILING_OPTION=""
 fi
 
-for ulysses_degree in $N_GPUS
-#for ulysses_degree in 1 2 4 8
-do
+if [[ "$SWEEP_BENCHMARK" == "1" ]]; then
+	time=$(date +%Y-%m-%d_%H:%M:%S)
+	LOGFILE=hyvideo_xdit_$time.log
+	for ulysses_degree in 1 2 4 8
+	do
+		if [[ "$ulysses_degree" == "1" ]]; then
+			vae_options=("")
+		else
+			vae_options=("--enable_tiling" "--enable_tiling --enable_slicing")
+			#vae_options=("" "--enable_tiling" "--enable_slicing" "--enable_tiling --enable_slicing")
+		fi
+		for ENABLE_TILING_SLICING in "${vae_options[@]}"
+		do
+			echo "ENABLE_TILING_SLICING: " $ENABLE_TILING_SLICING
+			offload_options=("" $ENABLE_MODEL_CPU_OFFLOAD)
+			for ENABLE_MODEL_CPU_OFFLOAD in "${offload_options[@]}"
+			do
+				echo "CPU_OFFLOAD: " $CPU_OFFLOAD
+				ring_degree=$(($N_GPUS/$ulysses_degree))
+				PARALLEL_ARGS="--ulysses_degree ${ulysses_degree} --ring_degree ${ring_degree}"
+				torchrun --nproc_per_node=$N_GPUS ./examples/$SCRIPT \
+					--model $MODEL_ID \
+					$PARALLEL_ARGS \
+					$TASK_ARGS \
+					$PIPEFUSION_ARGS \
+					$OUTPUT_ARGS \
+					--num_inference_steps $INFERENCE_STEP \
+					--warmup_steps $WARMUP_STEPS \
+					--prompt "A cat walks on the grass, realistic" \
+					$CFG_ARGS \
+					$PARALLLEL_VAE \
+					$ENABLE_TILING_SLICING \
+					$ENABLE_MODEL_CPU_OFFLOAD \
+					$COMPILE_FLAG \
+					$PROFILING_OPTION \
+					2>&1 | tee -a ./logs/$LOGFILE
+			done
+		done
+	done
+else
+	ulysses_degree=$N_GPUS
 	ring_degree=$(($N_GPUS/$ulysses_degree))
 	PARALLEL_ARGS="--ulysses_degree ${ulysses_degree} --ring_degree ${ring_degree}"
 	torchrun --nproc_per_node=$N_GPUS ./examples/$SCRIPT \
@@ -59,4 +99,4 @@ do
 		$ENABLE_MODEL_CPU_OFFLOAD \
 		$COMPILE_FLAG \
 		$PROFILING_OPTION
-done
+fi
